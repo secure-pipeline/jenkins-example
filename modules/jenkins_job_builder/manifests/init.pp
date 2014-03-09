26050a8e05858774383fe3a1e4b6ea5033213313
Gareth Rushgrove <gareth@morethanseven.net>
2014-03-09 18:07:19 +0000

    tidy up jjb manifest and include vendored template

--- modules/jenkins_job_builder/manifests/init.pp
+++ modules/jenkins_job_builder/manifests/init.pp
@@ -10,19 +10,7 @@ class jenkins_job_builder (
   $password = '',
 ) {
 
-  # A lot of things need yaml, be conservative requiring this package to avoid
-  # conflicts with other modules.
-  if ! defined(Package['python-yaml']) {
-    package { 'python-yaml':
-      ensure => present,
-    }
-  }
-
-  if ! defined(Package['python-jenkins']) {
-    package { 'python-jenkins':
-      ensure => present,
-    }
-  }
+  ensure_packages(['python-pip', 'python-yaml', 'python-jenkins'])
 
   vcsrepo { '/opt/jenkins_job_builder':
     ensure   => latest,
@@ -36,6 +24,7 @@ class jenkins_job_builder (
     path        => '/usr/local/bin:/usr/bin:/bin/',
     refreshonly => true,
     subscribe   => Vcsrepo['/opt/jenkins_job_builder'],
+    require     => Package['python-pip'],
   }
 
   file { '/etc/jenkins_jobs':
@@ -56,7 +45,7 @@ class jenkins_job_builder (
   file { '/etc/jenkins_jobs/jenkins_jobs.ini':
     ensure  => present,
     mode    => '0400',
-    content => template('jenkins/jenkins_jobs.ini.erb'),
+    content => template('jenkins_job_builder/jenkins_jobs.ini.erb'),
     require => File['/etc/jenkins_jobs'],
   }
 }
