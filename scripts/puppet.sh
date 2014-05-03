5da53c7480b3ac8f4520da4f9e84b8f07c2e2cc1
Gareth Rushgrove <gareth@morethanseven.net>
2014-05-03 18:15:23 +0100

    set ruby to 2.0

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -5,6 +5,14 @@ orange='\e[0;33m'
 green='\e[0;32m'
 end='\e[0m'
 
+# Ruby on Ubuntu is broken by design :(
+echo -e "----> ${green}Installing ruby${end}"
+sudo apt-get install ruby2.0 ruby2.0-dev
+for i in erb gem irb rake rdoc ri ruby testrb
+do
+  sudo ln -sf /usr/bin/${i}2.0 /usr/bin/${i}
+done
+
 # if puppet is already installed do nothing
 if which /usr/local/bin/puppet > /dev/null 2>&1; then
   echo -e "${orange}----> Puppet is aready installed${end}"
@@ -14,11 +22,6 @@ fi
 # Install language locale as without can 
 # interfere with package installation
 sudo apt-get install language-pack-en -y
-
-# Install Ruby
-echo -e "----> ${green}Installing ruby${end}"
-sudo apt-get update
-sudo apt-get install ruby ruby-dev -y
 
 # Install puppet/facter
 echo -e "----> ${green}Installing puppet${end}"
